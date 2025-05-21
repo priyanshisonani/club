f496721052da96e8bf3fcbcc69ac648fe6011785
priyanshisonani <144811999+priyanshisonani@users.noreply.github.com>
2025-05-21 21:00:30 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Twister</title>
+  <!-- bootstrap site -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+
+  <!-- favicon -->
+  <link rel="shortcut icon" href="images\logo-twister.png " type="image/x-icon">
+  <!-- extranal css file -->
+  <link rel="stylesheet" href="./mystyle.css">
+
+
+  <!-- fonticons -->
+  <link rel="stylesheet" href="fonts\font-awesome-4.7.0\css\font-awesome.min.css">
+  <style>
+
+  </style>
+</head>
+
+<body class="bg-img">
+   <div>  </div>
+</body>
+  
+</html>
